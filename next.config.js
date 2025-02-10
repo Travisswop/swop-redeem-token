27ad3a908a890cf8112884518e9ceb8584dcbe72
irakibul7 <irakibul568@gmail.com>
2025-02-10 13:11:17 +0600

    fix bug on token redeem

--- next.config.js
+++ next.config.js
@@ -5,6 +5,46 @@ const nextConfig = {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https', // Allow any protocol
+        hostname: '*', // Allow any hostname
+      },
+      {
+        protocol: 'http', // Allow any protocol
+        hostname: '*', // Allow any hostname
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'nftstorage.link',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ipfs.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.seadn.io',
+      },
+    ],
+    // domains: [
+    //   "*",
+    //   "i.seadn.io",
+    //   "ipfs.io",
+    //   "i.ibb.co",
+    //   "nftstorage.link",
+    //   "res.cloudinary.com",
+    // ], // Allow images from all domains
+  },
 };
 
 module.exports = nextConfig;
